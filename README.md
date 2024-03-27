5ec2b5ae8d142cb214ce4e7830c1ca4f7f997dca
Lucas García <lucasgarcia.developer@gmail.com>
2024-03-27 08:37:03 -0300

    Add variables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<div style="display: flex; align-items: center; gap: 20px">
+<div style="width: 100%; display: flex; align-items: center; gap: 20px">
   <img src="./logos/python-logo.png" width="50px">
   <h1 style="display: inline;">Learing Python<h1>
 </div>
---
+++ python-newby/variables.py
@@ -0,0 +1,27 @@
+# Comments
+# This is a lineal comment
+
+"""
+This is a 
+multilineal comment
+"""
+
+# Variables
+my_string = "I'm a string"
+my_number = 123
+my_float = 123.4
+
+
+# Console Output
+print(my_string)
+print(my_number)
+print(my_float)
+
+# Types of variables
+print(type(my_string))
+print(type(my_number))
+print(type(my_float))
+
+# set several variables at the same time
+a_number, a_string, a_float = 1, 'a string', 1.3
+print(a_number, a_string, a_float)
