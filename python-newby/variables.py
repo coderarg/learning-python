1d3a894d4b3640eaab802753217dfd80ca96ae4c
Lucas García <lucasgarcia.developer@gmail.com>
2024-03-27 09:07:20 -0300

    Update variables

--- python-newby/variables.py
+++ python-newby/variables.py
@@ -10,17 +10,21 @@ multilineal comment
 my_string = "I'm a string"
 my_number = 123
 my_float = 123.4
+my_complex = (1 + 1j)
 
 
 # Console Output
 print(my_string)
 print(my_number)
 print(my_float)
+print(my_complex)
+
 
 # Types of variables
-print(type(my_string))
-print(type(my_number))
-print(type(my_float))
+print(type(my_string)) #<class 'str'>
+print(type(my_number)) #<class 'int'>
+print(type(my_float)) #<class 'float'>
+print(type(my_complex)) #<class 'complex'>
 
 # set several variables at the same time
 a_number, a_string, a_float = 1, 'a string', 1.3
