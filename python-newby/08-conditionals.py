e03031d16e5c705b135d545be900f9dd931df993
LucasGarcia-Arkano <lucas.garcia@arkanosoft.com>
2024-03-31 17:46:36 -0300

    Add conditionals and loops

---
+++ python-newby/08-conditionals.py
@@ -0,0 +1,17 @@
+# Conditionals
+
+a = 5
+if (a < 0):
+    print('El número es menor a cero')
+elif (a > 0):
+    print('El número es mayor a cero')
+else:
+    print('El número es igual a cero')
+
+# Comparando tipo de datos
+a = 10
+b = 'pintura'
+if (type(a) == type(b)):
+    print('a y b son del mismo tipo de dato')
+else:
+    print('a y b son de tipos de dato diferentes')
