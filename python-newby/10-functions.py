cbf0783223fc755d254e4efd470e9a2bbd9ee4ca
LucasGarcia-Arkano <lucas.garcia@arkanosoft.com>
2024-03-31 18:01:27 -0300

    Update functions

--- python-newby/10-functions.py
+++ python-newby/10-functions.py
@@ -9,7 +9,18 @@ def sum_two_values (first, second):
     print(int(first) + int(second))
 sum_two_values (1, 2)
 
-def multiply_to_values (first, second = 10): #The second value has a default value
+def multiply_two_values (first, second = 10): #The second value has a default value
     return first * second
 
-print(multiply_to_values(3, 4))
+print(multiply_two_values(3, 4))
+
+def multiply_each_number (*numbers): # Apply the function to each element
+    product = 0
+    for number in numbers:
+        if product == 0: product = number
+        else: product *= number
+    
+    return product
+
+print(multiply_each_number(1, 3, 3, 3))
+
