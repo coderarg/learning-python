ed5ca535302a0c701403d564b8460faa7766ecae
LucasGarcia-Arkano <lucas.garcia@arkanosoft.com>
2024-04-04 21:49:15 -0300

    add fizz-buzz exercise

--- python-newby/03-strings.py
+++ python-newby/03-strings.py
@@ -47,4 +47,18 @@ print(language.count('t'))
 print("1".isnumeric()) #true
 
 # starts with
-print("Hola".startswith("Ho")) #true
+print("Hi".startswith("H")) #true
+
+# Trim a string
+#For whitespace on both sides, use str.strip():
+s = "  \t a string example\t  "
+s = s.strip()
+
+#For whitespace on the right side, use str.rstrip():
+s = s.rstrip()
+
+#For whitespace on the left side, use str.lstrip():
+s = s.lstrip()
+
+#You can provide an argument to strip arbitrary characters to any of these functions, like this:
+s = s.strip(' \t\n\r')
